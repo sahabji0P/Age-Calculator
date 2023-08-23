ae0a7ee57ff4e6bae33df9e17366271641314d11
Shashwat Jain <136179787+sahabji0P@users.noreply.github.com>
2023-08-24 00:57:15 +0530

    Imporved the code

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,14 +1,24 @@
+from datetime import date
+
 def calculate_age(Birth_year):
-  current_year=2023
+
+  current_year = date.today().year
+
   if Birth_year>current_year:
-    raise valueError("Invalid Birth year")
+    
+    raise ValueError("Invalid Birth year")
+  
   else:
+
     age=current_year-Birth_year
     return age  
 
 try:
+
+
   Year=int(input("Enter the birth year:"))
   age=calculate_age(Year)
   print("you are",age,"Years old")
+
 except:
   print("Invalid ")
